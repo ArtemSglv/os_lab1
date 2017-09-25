d86173ebbce8384c48de0e6db25ec193187ea177
Artem Shcheglov <stiw00@bk.ru>
2017-09-25 18:06:29 +0300

    почти все

--- lab1.sh
+++ lab1.sh
@@ -1,5 +1,31 @@
 #! /bin/bash
-awk '{if($11 != "\"-\"") print $7,$11 > "log.unordered"}' log.txt
+# поиск топовой url
+awk '{if($11 != "\"-\"")
+    {
+        print $7,$11 > "url_ref.txt";
+        url_list[$7]+=1;
+        print $7,url_list[$7];
+    }}' log.txt | sort -nrk 2 | awk '{if(NR==1) print > "top_url.txt"}'
+# составление топ 10 рефереров
 awk '{
-    url_list[$1]+=1; print $1,url_list[$1] > "url_list.txt"
-}' log.unordered
+        if(NR==1)
+           turl=$1
+        else
+        if($1==turl)
+        {
+            output[$2]+=1;
+            print $2,output[$2]
+        }
+}' top_url.txt url_ref.txt | sort -nrk 2 | awk '{if(NR<11) print}' | sed s/\"//g > top_ref.txt
+# сумма запросов с топовых рефереров
+awk '{sum+=$2} END {print sum > "sum"}' top_ref.txt
+# итоговый вывод
+awk '{
+    if(NR==1)
+        sum=$1
+    else    
+    {
+        ORS="%\n"
+        print $1,"-",$2,"-",$2/sum*100
+    }
+}' sum top_ref.txt
